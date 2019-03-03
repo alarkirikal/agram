42815feda359ef2234910416ecd49d172a8d1eea
Alar Kirikal <alar.kirikal@pipedrive.com>
2019-03-03 20:29:07 +0200

    Python what

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 from datetime import datetime
 from collections import Counter
 
@@ -5,28 +7,29 @@ import sys
 
 def anagrams():
     retval = []
+    append = retval.append
 
-    key = " ".join(sys.argv[2:])
-    arg_len = len(key) + 1
+    key = " ".join(sys.argv[2:]).replace("ž", "þ")
+    arg_len = len(key)
     arg_counter = Counter(key)
 
-    with open(sys.argv[1], encoding='ISO-8859-15') as file:
+    with open(sys.argv[1], encoding='iso-8859-15') as file:
         for line in file:
-            if len(line) != arg_len:
+            if len(line) - 1 != arg_len:
                 continue
 
             line = line.rstrip()
 
             if Counter(line) == arg_counter:
-                retval.append(line)
+                append(",%s" % line.replace("þ", "ž"))
 
-    return ",".join(retval)
+    return "".join(retval)
 
 
 if __name__ == "__main__":
     start = datetime.now()
 
     result = anagrams()
-
+    
     end = datetime.now()
-    print(str((end - start).microseconds) + "," + result)
+    print(str((end - start).microseconds) + result)
