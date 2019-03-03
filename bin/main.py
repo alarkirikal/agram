882b007aa7cd9f48115ef0bbf9da7759d00fe7be
Alar Kirikal <alar.kirikal@pipedrive.com>
2019-03-03 22:12:30 +0200

    Python Improvements

--- bin/main.py
+++ bin/main.py
@@ -1,35 +1,33 @@
-# -*- coding: UTF-8 -*-
-
 from datetime import datetime
 from collections import Counter
 
 import sys
+
 
 def anagrams():
     retval = []
     append = retval.append
 
     key = " ".join(sys.argv[2:]).replace("ž", "þ")
-    arg_len = len(key)
+    arg_len = len(key) + 1
     arg_counter = Counter(key)
 
     with open(sys.argv[1], encoding='iso-8859-15') as file:
         for line in file:
-            if len(line) - 1 != arg_len:
+            if len(line) != arg_len:
                 continue
 
             line = line.rstrip()
 
             if Counter(line) == arg_counter:
-                append(",%s" % line.replace("þ", "ž"))
+                append(line)
 
-    return "".join(retval)
+    return ",".join(retval).replace("þ", "ž")
 
 
 if __name__ == "__main__":
     start = datetime.now()
-
     result = anagrams()
-    
     end = datetime.now()
-    print(str((end - start).microseconds) + result)
+
+    print("{0},{1}".format(str((end - start).microseconds), result))
